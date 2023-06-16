0f4d9743f1774da9572e56389bc6f30981f3aab8
rishiraj007 <rishiraj@mounttalent.com>
2023-06-16 18:00:36 +0530

    [Prabhu]Add: Interface Concept

---
+++ PracticeProblem/IntefaceConcept.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface IShape
+    {
+        double CalculateArea();  // Method declaration
+        void Display();          // Method declaration
+    }
+
+    // Implement the interface in a class
+    public class Circle : IShape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Circle with radius {radius} and area {CalculateArea()}");
+        }
+    }
+
+    // Implement the interface in another class
+    public class Rectangle : IShape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double CalculateArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Rectangle with length {length}, width {width}, and area {CalculateArea()}");
+        }
+    }
+}
--- PracticeProblem/Program.cs
+++ PracticeProblem/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace PracticeProblem
+﻿using Interface;
+
+namespace PracticeProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice Problem !");
+            Console.WriteLine("Practice Problem - Interface Concept");
+            Circle circle = new Circle(5);
+            circle.Display();  
+
+            Rectangle rectangle = new Rectangle(4, 6);
+            rectangle.Display();  
         }
     }
 }
